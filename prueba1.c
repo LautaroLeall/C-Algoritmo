8520c98874592c526bafc593663488600ea13427
LautaroLeall <lautaroleal4@gmail.com>
2024-11-29 00:11:48 -0300

    prueba1.c

---
+++ prueba1.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+
+    // Imprime el mensaje "Hola Mundo"
+// int main() {
+//     printf("Hola Mundo");
+//     return 0;
+// }
+
+    // Imprime un caracter
+// int main(){
+//     char letra = 'L';
+//     printf("Mi nomrbre comienza con %c ", letra);
+//     return 0;
+// }
+
+    // Imprime un numero
+// int main(){
+//     int numero;
+//     printf("INGRESE UN NUMERO: ");
+//     scanf("%d", &numero);
+//     printf("EL NUMERO INGRESADO ES: %d ", numero);
+//     return 0;
+// }
+
+        // Operadores Aritmeticos
+
+    // Imprime Operadores con numero entero
+// int main(){
+//     int numero1, numero2;
+//     printf("INGRESE UN NUMERO: ");
+//     scanf("%d", &numero1);
+//     printf("INGRESE OTRO NUMERO: ");
+//     scanf("%d", &numero2);
+//     int suma = numero1 + numero2;
+//     int resta = numero1 - numero2 ;
+//     int multiplicacion = numero1 * numero2 ;
+//     int division = numero1 / numero2;
+//     int modulo = numero1 % numero2;
+//     printf("LA SUMA ES: %d \n", suma);
+//     printf("LA RESTA ES: %d \n", resta);
+//     printf("LA MULTIPLICACION ES: %d \n", multiplicacion);
+//     printf("LA DIVISION ES: %d", division);
+//     printf("EL MODULO ES: %d", modulo);
+//     return 0;
+// }
+
+    // Imprime Operadores con numero decimal
+// int main(){
+//     float numero1, numero2;
+//     printf("INGRESE UN NUMERO: ");
+//     scanf("%f", &numero1);
+//     printf("INGRESE OTRO NUMERO: ");
+//     scanf("%f", &numero2);
+//     float suma = numero1 + numero2;
+//     float resta = numero1 - numero2 ;
+//     float multiplicacion = numero1 * numero2 ;
+//     float division = numero1 / numero2;
+//     printf("LA SUMA ES: %f \n", suma);
+//     printf("LA RESTA ES: %f \n", resta);
+//     printf("LA MULTIPLICACION ES: %f \n", multiplicacion);
+//     printf("LA DIVISION ES: %f", division);
+//     return 0;
+// }
+
+        //Operadores Unarios
+
+    // Imprime Operadores Unarios
+// int main(){
+//     int numero;
+//     printf("INGRESE UN NUMERO: ");
+//     scanf("%d", &numero);    
+     // Se puede uno a la vez
+//     int incremento = --numero; 
+//     int decremento = ++numero ;
+//     printf("EL INCREMENTO DEL N INGRESADO ES: %d \n", incremento);
+//     printf("EL DECREMENTO DEL N INGRESADO ES: %d",  decremento);
+//     return 0;
+// }
+
+        //Operadores Relacionales
+
+    // Imprime Operadores Relacionales
+    // Resultados Binarios 1=Verdadero / 0=Falso
+// int main(){
+//     int numero1, numero2;
+//     printf("INGRESE UN NUMERO: ");
+//     scanf("%d", &numero1);
+//     printf("INGRESE OTRO NUMERO: ");
+//     scanf("%d", &numero2);
+//     int igualdad = (numero1 == numero2);
+//     int desigualdad = (numero1 != numero2);
+//     int mayor = (numero1 > numero2);
+//     int menor = (numero1 < numero2);
+//     int mayorigual = (numero1 >= numero2);
+//     int menorigual = (numero1 <= numero2);
+//     printf("LA IGUALDAD ES: %d \n", igualdad);
+//     printf("LA DESIGUALDAD ES: %d \n", desigualdad);
+//     printf("EL MAYOR ES: %d \n", mayor);
+//     printf("EL MENOR ES: %d \n", menor);
+//     printf("EL MAYOR O IGUAL ES: %d \n", mayorigual);
+//     printf("EL MENOR O IGUAL ES: %d", menorigual);
+//     return 0;
+// }
+
+    // Imprime Tmaño Minimo del Tipo de Dato
+// int main(){
+//     //PUEDE IR CUALQUIER TIPO DE DATO
+//     int a;
+//     printf("EL TAMANO MINIMO DEL TIPO DE DATO ES: %lu \n", sizeof(a));
+// }
+
